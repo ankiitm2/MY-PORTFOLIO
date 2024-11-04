37c75a4c0054843cb50779f96381db8fe447329b
Ankit Mishra <ankitmishra07am@gmail.com>
2024-11-04 15:23:59 +0530

    intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,22 +3,53 @@ import ThemeSwitcher from "./ThemeSwitcher";
 const Intro = () => {
   return (
     <>
-      <div className="navbar fixed bottom-5">
+      <div className="navbar fixed bottom-5 left-0 px-10">
         <div className="logo font-extrabold text-7xl">
           <h3>Ankit</h3>
           <h2 className="uppercase">Mishra</h2>
         </div>
-        <div className="details flex gap-12">
-          <div className="box flex flex-col uppercase mt-9 text-sm gap-2 font-thin">
-            <a href="#">frontend developer</a>
-            <a>ui developer</a>
+        <div className="details flex justify-between items-center">
+          <div className="boxes flex gap-12 uppercase mt-9 text-sm font-thin">
+            <div className="box flex flex-col  gap-2 text-xl">
+              <a href="#">frontend developer</a>
+              <a>ui developer</a>
+            </div>
+            <div className="box flex flex-col gap-2 ">
+              <a
+                href="mailto:mishraankittm2@gmail.com"
+                className="lowercase font-semibold"
+              >
+                mishraankittm2@gmail.com
+              </a>
+              <a>gurgaon, india</a>
+            </div>
           </div>
-          <div className="box flex flex-col uppercase mt-9 text-sm gap-2 font-thin">
-            <a href="mailto:mishraankittm2@gmail.com">
-              mishraankittm2@gmail.com
-            </a>
-            <a>gurgaon, india</a>
-          </div>
+          <ul className="social font-semibold list-circle">
+            <li>
+              <a
+                href="https://www.linkedin.com/in/ankit-mishra-3aab7b236"
+                target="_blank"
+              >
+                linkedin
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/ankit-mishra-3aab7b236"
+                target="_blank"
+              >
+                git hub
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/ankit-mishra-3aab7b236"
+                target="_blank"
+              >
+                instagram
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
       <ThemeSwitcher />
