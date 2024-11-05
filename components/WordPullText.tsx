0b7915784e832483499c353c3cfe2afb4ddb1826
Ankit Mishra <ankitmishra07am@gmail.com>
2024-11-05 18:39:02 +0530

    responsive

--- components/WordPullText.tsx
+++ components/WordPullText.tsx
@@ -5,7 +5,7 @@ export function WordPullUpDemo() {
   return (
     <>
       <WordPullUp
-        className="logo fixed bottom-20 w-1/5 text-4xl font-extrabold tracking-[-0.02em] text-black dark:text-white md:text-7xl md:leading-[5rem] text-left"
+        className="logo fixed bottom-20 w-1/5 text-3xl font-extrabold tracking-[-0.02em] text-black dark:text-white md:text-7xl md:leading-[5rem] text-left"
         words="Ankit Mishra"
       />
       <div className="responsive_desc fixed w-2/5 hidden text-xl">
