af48662e1b8bcf0f1a188855bfee2b33984a2e61
Ankit Mishra <ankitmishra07am@gmail.com>
2024-11-06 20:45:07 +0530

    redirection prob

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,14 @@
 import { AnimatedGridPatternDemo } from "@/components/GridBgText";
 import { WordPullUpDemo } from "@/components/WordPullText";
+import Link from "next/link";
 
 const About = () => {
   return (
     <>
-      <div className="page_logo fixed text-sm top-0">
-        <WordPullUpDemo />
+      <div className="page_logo">
+        <Link href="/home" passHref>
+          <WordPullUpDemo />
+        </Link>
       </div>
       <AnimatedGridPatternDemo />
     </>
